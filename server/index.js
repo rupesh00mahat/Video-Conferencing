a730588e9bc2f892d93f493a53131c38ab386053
rupesh00mahat <rupesh00mahat@gmail.com>
2025-02-16 12:26:28 +0545

    add new context and features

--- server/index.js
+++ server/index.js
@@ -2,11 +2,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {Server} = require('socket.io');
 
-const io = new Server();
+const io = new Server(
+   {
+      cors: true,
+   }
+);
 const app = express();
 
+const emailToSocketMapping = new Map();
+const socketToEmailMapping = new Map();
+
 app.use(bodyParser.json());
-io.on('connection', (socket) =>{});
+io.on('connection', (socket) =>{
+     console.log('New connection');
+     socket.on('join-room', (data)=>{
+        const {roomId, emailId} = data;
+        console.log('User', emailId, 'Joined Room', roomId);
+        emailToSocketMapping.set(emailId, socket.id);
+        socketToEmailMapping.set(socket.id, emailId);
+        socket.join(roomId);
+        socket.emit('joined-room', {roomId});
+        socket.broadcast.to(roomId).emit('user-joined', {emailId});
+     })
+
+     socket.on('call-user', (data)=>{
+      const {emailId, offer} = data;
+      const fromEmail = socketToEmailMapping.get(socket.id);
+      const socketId = emailToSocketMapping.get(emailId);
+      socket.to(socketId).emit('incoming-call', {from: fromEmail, offer})
+     })
+});
 
 app.listen(8000, () => {console.log('Http server running on PORT 8000')});
 io.listen(8001);
